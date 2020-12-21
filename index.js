32bff72556a46c26dbb58ede4de8ae4c2f9daa0f
Anthony <ruffina@evms.edu>
2020-12-21 17:33:28 -0500

    updates

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ const button = document.getElementById("button");
 const levels = document.querySelectorAll(".sacs__menu-level__wrapper");
 const docs = document.querySelectorAll('.sacs__menu__docs')
 
-// const levels = document.querySelectorAll(".sacs__menu-level");
 const plusMinusIcons = document.querySelectorAll('.sacs__plus-minus-toggle')
 
 const firstExToggle = document.getElementById('firstExToggle')
@@ -28,8 +27,9 @@ const fourthDocToggle = document.getElementById('fourthDocToggle')
 const fifthExToggle = document.getElementById('fifthExToggle')
 const fifthDocToggle = document.getElementById('fifthDocToggle')
 
-const drawerTrigger = document.getElementById('drawerTrigger')
+const closeMenu = document.getElementById('closeMenu')
 const triggerIcon = document.getElementById('triggerIcon')
+const openMenu = document.getElementById('openMenu')
 
 const sacsNav = document.querySelector('.sacs__navigate-articles')
 
@@ -40,90 +40,30 @@ function hideSacsNav() {
   }
 }
 
+hideSacsNav()
+
 function showParent() {
   console.log(this)
 }
 
-hideSacsNav()
+function showOpenButton() {
+    openMenu.style.visibility = 'visible'
+}
 
-drawerTrigger.addEventListener('click', function() {
-  
-  // sidenav.classList.toggle('hide-menu')
-  sidenav.style.width = '10px'
-  triggerIcon.innerHTML = 'chevron_right'
-  drawerTrigger.style.left = '80% !important'
-  
-  if (triggerIcon.innerHTML === 'chevron_right' && sidenav.style.width >= 400) {
-    sidenav.style.width = '440px'
-  } 
+function hideOpenButton() {
+  openMenu.style.visibility = 'hidden'
+}
+
+openMenu.addEventListener('click', function() {
+  main.style.marginLeft = '440px'
+  hideOpenButton()
 })
 
-// first level of nesting
-// const subLevels = {
-//   levelOne: document.getElementById("subLevelOne"),
-//   levelTwo: document.getElementById('subLevelTwo'),
-//   levelThree: document.getElementById('subLevelThree'),
-//   levelFour: document.getElementById('subLevelFour'),
-//   levelFive: document.getElementById('subLevelFive'),
-//   levelSix: document.getElementById('subLevelSix'),
-//   levelSeven: document.getElementById('subLevelSeven'),
-//   levelEight: document.getElementById('subLevelEight'),
-//   levelNine: document.getElementById('subLevelNine'),
-//   levelTen: document.getElementById('subLevelTen'),
-//   levelEleven: document.getElementById('subLevelEleven'),
-// }
-
-// firstExToggle.addEventListener('click', function() {
-//   firstExToggle.innerHTML = 'remove'
-//   firstDocToggle.classList.toggle("docs-menu__open")
-
-//   if(!firstDocToggle.classList.contains('docs-menu__open')) {
-//     firstExToggle.innerHTML = 'add'
-//   }
-// })
-
-// secExToggle.addEventListener('click', function() {
-//   secExToggle.innerHTML = 'remove'
-
-//   secDocToggle.classList.toggle("docs-menu__open")
-  
-//   if(!secDocToggle.classList.contains('docs-menu__open')) {
-//     secExToggle.innerHTML = 'add'
-//   }
-// })
-
-// thirdExToggle.addEventListener('click', function() {
-//   thirdExToggle.innerHTML = 'remove'
-
-//   thirdDocToggle.classList.toggle("docs-menu__open")
-  
-//   if(!thirdDocToggle.classList.contains('docs-menu__open')) {
-//     thirdExToggle.innerHTML = 'add'
-//   }
-// })
-
-// fourthExToggle.addEventListener('click', function() {
-//   fourthExToggle.innerHTML = 'remove'
-
-//   fourthDocToggle.classList.toggle("docs-menu__open")
-  
-//   if(!fourthDocToggle.classList.contains('docs-menu__open')) {
-//     fourthExToggle.innerHTML = 'add'
-//   }
-// })
-
-// fifthExToggle.addEventListener('click', function() {
-//   fifthExToggle.innerHTML = 'remove'
-
-//   fifthDocToggle.classList.toggle("docs-menu__open")
-  
-//   if(!fifthDocToggle.classList.contains('docs-menu__open')) {
-//     fifthExToggle.innerHTML = 'add'
-//   }
-// })
-
-// for init build
-// main.style.marginLeft = menuWidth + "px";
+closeMenu.addEventListener('click', function() {
+  closeMenu.style.left = '80% !important'
+  main.style.marginLeft = '0px'
+  showOpenButton()
+})
 
 levels.forEach(function (level) {
   level.addEventListener("click", function () {
@@ -137,18 +77,13 @@ levels.forEach(function (level) {
   });
 });
 
-// function showNestedLevel() {
-  
-// }
-
 plusMinusIcons.forEach(function(icon) {
   icon.addEventListener('click', function() {
     icon.classList.toggle('collapsed')
-    showLevel(this.nextElementSibling.nextElementSibling)
+    console.log(this.parentNode.nextElementSibling)
+    showLevel(this.parentNode.nextElementSibling)
   })
 })
-
-// button.addEventListener("click", showNav);
 
 function hideNav() {
   sidenav.style.maxWidth = "0";
@@ -156,7 +91,6 @@ function hideNav() {
 
 function showNav() {
   sidenav.style.maxWidth = menuWidth + "px";
-  // main.style.marginLeft = menuWidth + "px";
 }
 
 function showLevel(subLevel) {
@@ -166,109 +100,3 @@ function showLevel(subLevel) {
 function hideLevel(subLevel) {
   subLevel.style.maxHeight = subLevelInitHeight + 'px';
 }
-
-
-
-// function hideSubLevel(id) {
-//   switch (id) {
-//     case (id = "menuLevelOne"):
-//       hideLevel(subLevels.levelOne);
-//       break;
-
-//     case (id = "menuLevelTwo"):
-//       hideLevel(subLevels.levelTwo);
-//       break;
-
-//     case (id = "menuLevelThree"):
-//       hideLevel(subLevels.levelThree);
-//       break;
-
-//     case (id = "menuLevelFour"):
-//       hideLevel(subLevels.levelFour);
-//       break;
-
-//     case (id = "menuLevelFive"):
-//       hideLevel(subLevels.levelFive);
-//       break;
-
-//     case (id = "menuLevelSix"):
-//       hideLevel(subLevels.levelSix);
-//       break;
-
-//     case (id = "menuLevelSeven"):
-//       hideLevel(subLevels.levelSeven);
-//       break;
-
-//     case (id = "menuLevelEight"):
-//       hideLevel(subLevels.levelEight);
-//       break;
-
-//     case (id = "menuLevelNine"):
-//       hideLevel(subLevels.levelNine);
-//       break;
-
-//     case (id = "menuLevelTen"):
-//       hideLevel(subLevels.levelTen);
-//       break;
-
-//     case (id = "menuLevelEleven"):
-//       hideLevel(subLevels.levelEleven);
-//       break;
-
-//     default:
-//       break;
-//   }
-// }
-
-// function showSubLevel(id) {
-//   switch (id) {
-//     case (id = "menuLevelOne"):
-//       console.log("first called");
-//       showLevel(subLevels.levelOne);
-//       break;
-
-//     case (id = "menuLevelTwo"):
-//       showLevel(subLevels.levelTwo);
-//       break;
-
-//     case (id = "menuLevelThree"):
-//       showLevel(subLevels.levelThree);
-//       break;
-
-//     case (id = "menuLevelFour"):
-//       showLevel(subLevels.levelFour);
-//       break;
-
-//     case (id = "menuLevelFive"):
-//       showLevel(subLevels.levelFive);
-//       break;
-
-//     case (id = "menuLevelSix"):
-//       showLevel(subLevels.levelSix);
-//       break;
-
-//     case (id = "menuLevelSeven"):
-//       showLevel(subLevels.levelSeven);
-//       break;
-
-//     case (id = "menuLevelEight"):
-//       showLevel(subLevels.levelEight);
-//       break;
-
-//     case (id = "menuLevelNine"):
-//       showLevel(subLevels.levelNine);
-//       break;
-
-//     case (id = "menuLevelTen"):
-//       showLevel(subLevels.levelTen);
-//       break;
-
-//     case (id = "menuLevelEleven"):
-//       showLevel(subLevels.levelEleven);
-//       break;
-
-//     default:
-//       break;
-//   }
-// }
-
