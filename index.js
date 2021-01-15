dc97bfe8c12e25008b0379b59fa824358caa6441
Anthony <ruffina@evms.edu>
2021-01-15 15:06:10 -0500

    updates

--- index.js
+++ index.js
@@ -1,58 +1,56 @@
-const menuWidth = 500
+const menuWidth = 500;
 
-const main = document.getElementById("main")
+const main = document.getElementById("main");
 
-const levels = document.querySelectorAll(".sacs__menu-level__wrapper")
-const plusMinusIcons = document.querySelectorAll('.sacs__plus-minus-toggle')
+const levels = document.querySelectorAll(".sacs__menu-level__wrapper");
+const plusMinusIcons = document.querySelectorAll(".sacs__plus-minus-toggle");
 
-const openMenu = document.getElementById('openMenu')
-const closeMenu = document.getElementById('closeMenu')
+const openMenu = document.getElementById("openMenu");
+const closeMenu = document.getElementById("closeMenu");
 
 function showOpenButton() {
-  openMenu.style.visibility = 'visible'
-  closeMenu.style.visibility = 'hidden'
+  openMenu.style.visibility = "visible";
+  closeMenu.style.visibility = "hidden";
 }
 
 function hideOpenButton() {
-  openMenu.style.visibility = 'hidden'
-  closeMenu.style.visibility = 'visible'
+  openMenu.style.visibility = "hidden";
+  closeMenu.style.visibility = "visible";
 }
 
 function showLevel(subLevel, icon) {
-  subLevel.classList.toggle('show-level')
-  icon.firstElementChild.innerHTML = 'remove'
+  subLevel.classList.toggle("show-level");
+  icon.firstElementChild.innerHTML = "remove";
 
-  if(!subLevel.classList.contains('show-level')) {
-    icon.firstElementChild.innerHTML = 'add'
+  if (!subLevel.classList.contains("show-level")) {
+    icon.firstElementChild.innerHTML = "add";
   }
 }
 
-openMenu.addEventListener('click', function() {
-  main.style.marginLeft = '440px'
-  hideOpenButton()
-})
+openMenu.addEventListener("click", function () {
+  main.style.marginLeft = "440px";
+  hideOpenButton();
+});
 
-closeMenu.addEventListener('click', function() {
-  closeMenu.style.left = '80% !important'
-  main.style.marginLeft = '0px'
-  showOpenButton()
-})
+closeMenu.addEventListener("click", function () {
+  closeMenu.style.left = "80% !important";
+  main.style.marginLeft = "0px";
+  showOpenButton();
+});
 
 levels.forEach(function (level) {
   level.addEventListener("click", function () {
-    level.classList.toggle("sacs__menu__opened")
-    showLevel(this.nextElementSibling)
+    level.classList.toggle("sacs__menu__opened");
+    showLevel(this.nextElementSibling);
+  });
+});
 
-    if (!level.classList.contains("sacs__menu__opened")) {
-      hideSubLevel(this.id)
-    }
-  })
-})
-
-plusMinusIcons.forEach(function(icon) {
-  icon.addEventListener('click', function() {
-    icon.classList.toggle('collapsed')
-    showLevel(this.parentElement.nextElementSibling, this.parentElement.nextElementSibling.parentElement.firstElementChild)
-  })
-})
-
+plusMinusIcons.forEach(function (icon) {
+  icon.addEventListener("click", function () {
+    icon.classList.toggle("collapsed");
+    showLevel(
+      this.parentElement.nextElementSibling,
+      this.parentElement.nextElementSibling.parentElement.firstElementChild
+    );
+  });
+});
